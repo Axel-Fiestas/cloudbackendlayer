0e1bb9fd6ff222c061254edacdabb6b78def6d4c
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2024-06-02 12:17:05 -0500

    feat: extra config

--- src/main/java/com/upc/cloudbackendlayer/CloudWebSocketConfig.java
+++ src/main/java/com/upc/cloudbackendlayer/CloudWebSocketConfig.java
@@ -1,7 +1,11 @@
 package com.upc.cloudbackendlayer;
 
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -21,5 +25,17 @@ public class CloudWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://html-web-socket-example-iot.vercel.app"); // Reemplaza esto con tu URL de Vercel
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter();
     }
 }

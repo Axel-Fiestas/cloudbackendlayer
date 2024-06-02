437141a5646749bf8f48343fbb28139e6c3e0ced
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2024-06-02 11:20:28 -0500

    Initial commit

---
+++ src/test/java/com/upc/cloudbackendlayer/CloudbackendlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.cloudbackendlayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudbackendlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
